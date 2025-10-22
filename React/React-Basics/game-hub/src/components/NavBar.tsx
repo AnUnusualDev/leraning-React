ca7c3d509eee5f31b54764d7eaa5e6908dc9315e
Jonas Miejki <jonas.miejski@gmail.com>
2025-10-22 16:46:27 +0200

    implement everything until darkmode

---
+++ React/React-Basics/game-hub/src/components/NavBar.tsx
@@ -0,0 +1,8 @@
+import { Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+const NavBar = () => {
+  return <Image src={logo} boxSize="60px"></Image>;
+};
+
+export default NavBar;
