203297c67e86a2ba8a6b5dc7fb2d6eca0a494f51
Jonas Miejki <jonas.miejski@gmail.com>
2025-10-28 01:13:18 +0100

    Clean up the game cards

--- React/React-Basics/game-hub/src/components/GameCard.tsx
+++ React/React-Basics/game-hub/src/components/GameCard.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { Game } from "../hooks/useGames";
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
@@ -13,13 +12,13 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           ></PlatformIconList>
           <CriticScore score={game.metacritic}></CriticScore>
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
