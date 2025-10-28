b79acb73ee9864036433751e2b0bff8086711033
Jonas Miejki <jonas.miejski@gmail.com>
2025-10-28 16:32:46 +0100

    Installed Chakra

--- React/Todo-List/src/App.tsx
+++ React/Todo-List/src/App.tsx
@@ -1,7 +1,14 @@
+import { Text, Tooltip } from "@chakra-ui/react";
 import "./App.css";
+import { ColorModeButton } from "./components/ui/color-mode";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Text>Hello World</Text>
+      <ColorModeButton />
+    </>
+  );
 }
 
 export default App;
