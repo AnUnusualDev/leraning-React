152f6da9db3644d7deed1c159a413ddba9e50c7c
Jonas Miejki <jonas.miejski@gmail.com>
2026-01-29 23:04:10 +0100

    Disable prisma for localhost

--- NextJS/next-app/prisma/client.ts
+++ NextJS/next-app/prisma/client.ts
@@ -2,6 +2,11 @@ import { PrismaClient } from "@/app/generated/prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  // Während des Vercel-Builds (CI) geben wir ein leeres Objekt zurück
+  (process.env.VERCEL && !process.env.DATABASE_URL?.includes("localhost")
+    ? ({} as PrismaClient)
+    : new PrismaClient());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
